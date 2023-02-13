646db34af93b7948914b44fa1331fa9bea38aae2
shayanafkane <shayanafkane@gmail.com>
2023-02-13 15:33:43 +0330

    update

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { addtodo } from '../features/todo';
 
 function AddTodo() {
-    const [todo, setTodo] = useState();
+    const [todo, setTodo] = useState('');
     const dispatch = useDispatch({})
     const addTodo = () => {
         {
@@ -17,8 +17,18 @@ function AddTodo() {
     }
     return (
         <div className='container mx-auto text-center mt-8'>
-            <input value={todo} type="text" placeholder='Please enter your todo....' onChange={(event) => setTodo(event.target.value)} className='border p-2 rounded-l-md md:w-96' />
-            <button className='bg-teal-500 p-2 rounded-r-md' onClick={() => addTodo()}>Add</button>
+            <input value={todo} type="text" onKeyPress={(e) => {
+                if (e.key === "Enter") {
+                    if (e.target.value != '') {
+                        addTodo()
+                    }
+                }
+            }} placeholder='Please enter your todo....' onChange={(event) => setTodo(event.target.value)} className='border p-2 rounded-l-md md:w-96' />
+            <button className='bg-teal-500 p-2 rounded-r-md' onClick={() => {
+                if (todo != '') {
+                    addTodo()
+                }
+            }}>Add</button>
         </div>
     )
 }
