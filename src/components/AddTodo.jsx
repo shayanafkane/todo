b2c18ba94abc0fc5ca23d0c3722f5af2fb4f1e2f
shayanafkane <shayanafkane@gmail.com>
2023-02-13 12:50:47 +0330

    update

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function AddTodo() {
     return (
         <div className='container mx-auto text-center mt-8'>
-            <input type="text" placeholder='Please enter your Todo ....' className='border p-2 rounded-l-md w-96' />
+            <input type="text" placeholder='Please enter your todo....' className='border p-2 rounded-l-md w-96' />
             <button className='bg-teal-500 p-2 rounded-r-md'>Add</button>
         </div>
     )
--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import AddTodo from './AddTodo'
+import ListTodo from './ListTodo'
 
 function MainPage() {
     return (
         <div>
             <h1 className='text-center mt-4 text-4xl font-bold text-gray-600'>Todo App</h1>
             <AddTodo />
+            <ListTodo />
         </div>
     )
 }
